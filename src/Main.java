e7b60e5bfa628c497b4e6c185aa1f6bf40e22a7d
KarpovaO <grose@yandex.ru>
2021-09-12 22:49:27 +0300

    Miles

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 10000000;
+        int bonusMile = ticketPrice / 20;
+        System.out.println(bonusMile);
+    }
+}
